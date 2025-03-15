69354563a63ca52a6b86029af2367f196c8a529d
Mateusz Bilicz <mateuszbilicz@gmail.com>
2025-03-15 12:43:34 +0100

    implemented 404 page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit, signal, ViewEncapsulation} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
   imports: [],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  styleUrl: './not-found.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
-export class NotFoundComponent {
+export class NotFoundComponent
+  implements OnInit {
+  route = signal<string>('');
+  protected readonly activatedRoute = inject(ActivatedRoute);
+  protected readonly destroyRef = inject(DestroyRef);
 
+  ngOnInit() {
+    this.activatedRoute.url
+      .pipe(
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe(url => {
+        this.route.set(
+          url.join('/')
+        );
+      });
+  }
 }
